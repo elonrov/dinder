20fdcb1877da268cacae792356d3445915bacbc4
Elon <elonrov@gmail.com>
2020-02-24 13:54:32 -0500

    Create skeletons for  app.js and root.js

---
+++ frontend/src/components/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Switch } from 'react-router-dom';
+
+import SplashContainer from './splash/splash_container';
+
+const App = () => (
+    <div>
+        <SplashContainer />
+    </div>
+); 
+
+export default App;
