72cd1e1d7bb273f8f8c6f0f05bd4a272d31329d6
Elon <elonrov@gmail.com>
2020-02-25 10:53:23 -0500

    restructure of frontend render thread

---
+++ frontend/src/components/session_form/session_form.js
@@ -0,0 +1,17 @@
+import React from 'react'; 
+
+class SessionForm extends React.Component {
+    constructor(props) {
+        super(props); 
+    }
+
+    render () {
+        return (
+            <div>
+                Session Form
+            </div>
+        )
+    }
+}; 
+
+export default SessionForm; 
--- frontend/src/reducers/session_errors_reducer.js
+++ frontend/src/reducers/session_errors_reducer.js
@@ -1,9 +1,9 @@
-import { RECEIVE_SESSION_ERRORS } from "../actions/session_actions";
+import { RECEIVE_SESSION_ERRORS, CLEAR_ERRORS } from "../actions/session_actions";
 
 const sessionErrorsReducer = (state=[], action) => {
   switch (action.type) {
     case RECEIVE_SESSION_ERRORS:
-      action.errors;
+      return action.errors;
     case CLEAR_ERRORS:
       return [];
     default:
