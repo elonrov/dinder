41427ec501c13e628dc85bebab567b3e02c858d3
anicholas4747 <an1720@nyu.edu>
2020-02-25 14:18:00 -0500

    cards styling

---
+++ frontend/src/components/matching_round/matching_round_container.js
@@ -0,0 +1,12 @@
+import { connect } from 'react-redux';
+import MatchingRound from './matching_round';
+
+const mSTP = (state) => ({
+
+});
+
+const mDTP = (dispatch) => ({
+
+});
+
+export default connect(mSTP,mDTP)(MatchingRound);
