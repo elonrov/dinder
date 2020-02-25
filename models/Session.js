308c9b7069411dae7bed7eb9b66785efee5b1dbb
anicholas4747 <an1720@nyu.edu>
2020-02-25 09:08:06 -0500

    session schema

--- models/Session.js
+++ models/Session.js
@@ -2,11 +2,33 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const SessionSchema = new Schema({
+    numUsers: {
+        type: Number,
+        required: true
+    },
+    completedUsers: {
+        type: Array
+    },
+    choices: {
+        type: Array,
+        default: [
+            "tacos",
+            "pizza",
+            "sushi",
+            "thai",
+            'burgers',
+            "soup dumplings",
+            "subs",
+            "bbq",
+            "pho",
+            "ramen",
+            "tapas"
+        ]
+    },
     date: {
         type: Date, 
         default: Date.now
-    }, 
-    
+    }
 });
 
 module.exports = Session = mongoose.model('sessions', SessionSchema);
--- routes/api/sessions.js
+++ routes/api/sessions.js
@@ -1,6 +1,24 @@
 const express = require("express");
 const router = express.Router();
+const Session = require("../../models/Session");
 
 router.get("/test", (req, res) => res.json({ msg: "This is the sessions route" }));
+
+router.post("/session/new", (req, res) => {
+  const newSess = new Session(); // choices array should auto populate unless otherwise specified
+
+  newSess
+    .save()
+    .then(session => res.json(session));
+});
+
+router.patch("/session/:sessionId", (req, res) => {
+  const session = Session.findOne({ id: req.params.sessionId});
+  if(session){
+    session
+      .update({ completedUsers: req.params.completedUsers})
+      .then(session => res.json(session));
+  }
+});
 
 module.exports = router;
