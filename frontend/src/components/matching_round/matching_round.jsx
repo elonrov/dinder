c213b5248389e07e0f80db068bfce3e12fb6b8a2
anicholas4747 <an1720@nyu.edu>
2020-02-26 11:52:26 -0500

    connecting front to backend

--- frontend/src/components/matching_round/matching_round.jsx
+++ frontend/src/components/matching_round/matching_round.jsx
@@ -6,10 +6,16 @@ class MatchingRound extends Component{
     super(props);
     this.handleX = this.handleX.bind(this);
     this.handleCheck = this.handleCheck.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.rejections = [];
+    this.completedUsers = [];
   }
 
   handleX(e){
     e.preventDefault();
+    const newRejs = [...this.rejections, e.target.parentElement.previousElementSibling.firstElementChild.textContent];
+    this.rejections = newRejs;
+    
     e.target.parentElement.previousElementSibling.setAttribute("id", "HIDDEN-LEFT");
     e.target.parentElement.nextElementSibling.setAttribute("id", "BYE-LI");
     e.target.parentElement.setAttribute("id", "BYE-LI");
@@ -22,7 +28,21 @@ class MatchingRound extends Component{
     e.target.parentElement.setAttribute("id", "BYE-LI");
   }
 
+  handleSubmit(e){
+    e.preventDefault();
+  }
+
+  componentDidMount(){
+    if(this.props.session === undefined){
+      const sessionId = this.props.history.location.search.split(1);
+      this.props.fetchSession(sessionId);
+    }
+  }
+
   render(){
+
+    if(this.props.session === undefined) return <h1>Loading Session...</h1>;
+
     const cats = [
       "tacos",
       "pizza",
@@ -37,24 +57,50 @@ class MatchingRound extends Component{
       "tapas"
     ];
 
-    const cards = cats.map((food) => {
-      return (
-        <li key={`${food}${Date.now()}`} className="cards">
-          <span className="food-info">
-            <h2>{food}</h2>
-            <h3>New York, NY</h3>
-            <p>Here are some details about the food. I mean I dunno what should go here but I also couldn't find lorem ipsum so this is whatchu got.</p>
+    const cards = cats.reverse().map((food, idx) => {
+      if(idx === cats.length - 1){
+        return (
+          <span>
+            <li key={`LAST${Date.now()}`} className="cards" id="last-card">
+              <span className="food-info">
+                <h2>DONE!</h2>
+                <h3>Thank you for participating!</h3>
+                <p>Please wait for the others to finish, an email will be sent out with the final decision within the hour</p>
+                <button onClick={this.handleSubmit} id="submit-session">Submit Choices</button>
+              </span>
+            </li>
+            <li key={`${food}${Date.now()}`} className="cards">
+              <span className="food-info">
+                <h2>{food}</h2>
+                <h3>New York, NY</h3>
+                <p>Here are some details about the food. I mean I dunno what should go here but I also couldn't find lorem ipsum so this is whatchu got.</p>
+              </span>
+              <button className="x-out" onClick={this.handleX}><img src={window.xMark} alt="x-mark"/></button>
+              <button className="check" onClick={this.handleCheck}><img src={window.checkMark} alt="check-mark"/></button>
+            </li>
           </span>
-          <button className="x-out" onClick={this.handleX}><img src={window.xMark} /></button>
-          <button className="check" onClick={this.handleCheck}><img src={window.checkMark} /></button>
-        </li>
-      )
-    });
+        )
+      } else {
+        return (
+          <li key={`${food}${Date.now()}`} className="cards">
+            <span className="food-info">
+              <h2>{food}</h2>
+              <h3>New York, NY</h3>
+              <p>Here are some details about the food. I mean I dunno what should go here but I also couldn't find lorem ipsum so this is whatchu got.</p>
+            </span>
+            <button className="x-out" onClick={this.handleX}><img src={window.xMark} alt="x-mark"/></button>
+            <button className="check" onClick={this.handleCheck}><img src={window.checkMark} alt="check-mark"/></button>
+          </li>
+        )
+      }
+    }).reverse();
 
     return (
       <div className="matching-round">
         <h1>Matching Round</h1>
-        <ul>{cards}</ul>
+        <ul>
+          {cards}
+        </ul>
       </div>
       )
   }
--- frontend/src/components/matching_round/matching_round_container.js
+++ frontend/src/components/matching_round/matching_round_container.js
@@ -1,12 +1,14 @@
 import { connect } from 'react-redux';
 import MatchingRound from './matching_round';
+import { fetchSession } from '../../actions/session_actions';
 
 const mSTP = (state) => ({
-
+  session: state.session
 });
 
 const mDTP = (dispatch) => ({
-
+  fetchSession: (sessionId) => dispatch(fetchSession(sessionId)),
+  
 });
 
 export default connect(mSTP,mDTP)(MatchingRound);
