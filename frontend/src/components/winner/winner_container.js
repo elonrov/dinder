9574cdd35e6a2a218406189966b0a016355bafd7
Elon <elonrov@gmail.com>
2020-02-27 14:46:24 -0500

    winner component with winner picking render

--- frontend/src/components/winner/winner_container.js
+++ frontend/src/components/winner/winner_container.js
@@ -1,16 +1,19 @@
 import { connect } from 'react-redux';
-import { fetchSession } from '../../actions/session_actions';
+import { fetchSession, updateSession } from '../../actions/session_actions';
 import Winner from './winner';
 
-const mapStateToProps = state => {
+const mapStateToProps = (state, ownProps) => {
     return {
-        // winner: this.state.session.winner
+        session: this.state.session,
+        winner: this.state.session.winner, 
+        //make sure winner is added to session schema
     }
 };
 
 const mapDispatchToProps = dispatch => {
     return {
-        fetchSession: (sessionId) => dispatch(fetchSession(sessionId))
+        // fetchSession: (sessionId) => dispatch(fetchSession(sessionId)), 
+        updateWinner: (winner) => dispatch(updateSession(winner))
     }
 };
 
