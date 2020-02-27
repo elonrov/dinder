3ca1e20797c98ddfe29824f4b9db914f4e9fc39c
anicholas4747 <an1720@nyu.edu>
2020-02-27 18:24:30 -0500

    started yelp frontend comps

---
+++ frontend/src/components/session_round/restaurants /restaurants_round_container.js
@@ -0,0 +1,22 @@
+import { connect } from 'react-redux';
+import SessionRound from './session_round';
+import { fetchSession, updateSession } from '../../actions/session_actions';
+import { updateUser, verifyUser } from '../../actions/users_actions';
+
+const mSTP = (state) => {
+  debugger
+  return {
+    session: state.session,
+    currentUser: state.currentUser,
+    errors: state.errors.user
+  }
+};
+
+const mDTP = (dispatch) => ({
+  fetchSession: (sessionId) => dispatch(fetchSession(sessionId)),
+  updateSession: (sessionData) => dispatch(updateSession(sessionData)),
+  updateUser: (userData) => dispatch(updateUser(userData)),
+  verifyUser: (userData) => dispatch(verifyUser(userData))
+});
+
+export default connect(mSTP,mDTP)(SessionRound);
