810f30500f2f4f991bfb7163c36b9e514af741e0
ccurnuck <calvin.curnuck@gmail.com>
2020-02-28 18:03:08 -0500

    Modified reducer/api

--- frontend/src/reducers/users_reducer.js
+++ frontend/src/reducers/users_reducer.js
@@ -1,12 +1,10 @@
-import { RECEIVE_USER, RECEIVE_SESSION_USERS } from "../actions/users_actions";
+import { RECEIVE_SESSION_USERS, CLEAR_USER } from "../actions/users_actions";
 
 const usersReducer = (state=[], action) => {
   Object.freeze(state);
-  const newState = state;
   switch (action.type) {
-    case RECEIVE_USER:
-      newState.push(action.user);
-      return newState;
+    case CLEAR_USER:
+      return [];
     case RECEIVE_SESSION_USERS:
       return action.users
     default:
