9c873d013979496ef4830b164b2258a1300aaebf
Elon <elonrov@gmail.com>
2020-02-28 10:26:14 -0500

    app js edit for heroku

--- app.js
+++ app.js
@@ -22,6 +22,14 @@ app.use(bodyParser.json());
 app.use("/api/users", users);
 app.use("/api/session", sessions);
 
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'));
+    app.get('/', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    })
+}
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
