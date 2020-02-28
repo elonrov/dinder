7b149c0ff240be07a1eabebe7fa96701402ff781
Elon <elonrov@gmail.com>
2020-02-28 12:13:18 -0500

    session round redirect

--- frontend/src/components/session_round/session_round.jsx
+++ frontend/src/components/session_round/session_round.jsx
@@ -31,7 +31,7 @@ class SessionRound extends Component{
     const session = this.props.session;
     this.props.verifyUser(userVerificationData) // should update state to currentUser but will test
       .then (user => { 
-        if (!session.completedUsers.includes(user.email)) {
+        if (session.completedUsers.includes(user.email)) {
           this.props.history.push(`/sessions/${session._id}/winner`);
         }
       });
