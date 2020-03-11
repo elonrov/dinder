570d41bd0dc9a1af6da6adfca57c948ae8a6d6cf
Elon <elonrov@gmail.com>
2020-03-11 11:10:30 -0400

    assets for readme

--- routes/api/users.js
+++ routes/api/users.js
@@ -97,8 +97,6 @@ router.post("/create",
       .then(() => transporter.sendMail(mailOptions, function(err, data) {
         if (err) {
             console.log('Error Occurs', err)
-        } else {
-            // console.log('Email sent!!!')
         }
     }) )
       .catch((errors) => console.log('you have errors!', errors)) 
