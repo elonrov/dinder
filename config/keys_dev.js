a1d237d69c1a09c52b430a81b5c57536e30f7ee5
Elon <57840058+elonrov@users.noreply.github.com>
2020-03-19 12:41:47 -0400

    Delete keys_dev.js

--- config/keys_dev.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    mongoURI: 'mongodb+srv://dev:77oson33DbSmpWBc@cluster0-8g9oi.mongodb.net/test?retryWrites=true&w=majority',
-    EMAIL: 'dinderappaa@gmail.com',
-    PASSWORD: 'dinderteam123',
-    DINDER_SECRET_KEY: 'ASrxkXq7E1e03yTo7GwrubcIOmVDNwdcAOt126HI4ZtRXy1SqVchoahNfVAF03uWkGkguutxU2DuD2LzGqzw7JuZc_LKWxBrj0YnKqVE0LXF-pu4tYKQNl7CpgZYXnYx',
-    DINDER_SECRET_SAUCE: 'https://api.yelp.com/v3/businesses/search'
-}
