fc01f1e15fbbbf3a52303748c21bf245e3d6a622
Elon <57840058+elonrov@users.noreply.github.com>
2020-03-19 12:41:39 -0400

    Delete keys.js

--- config/keys.js
+++
@@ -1,7 +0,0 @@
-if (process.env.NODE_ENV === 'production') {
-    const keys_prod = require('./keys_prod');
-    module.exports = keys_prod;
-} else {
-    const keys_dev = require('./keys_dev');
-    module.exports = keys_dev;
-};
