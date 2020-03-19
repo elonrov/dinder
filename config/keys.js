c42e37b2acb1c134717649554c444782c3504059
Elon <elonrov@gmail.com>
2020-03-19 12:43:57 -0400

    new config

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+if (process.env.NODE_ENV === 'production') {
+  const keys_prod = require('./keys_prod');
+  module.exports = keys_prod;
+} else {
+  const keys_dev = require('./keys_dev');
+  module.exports = keys_dev;
+};
